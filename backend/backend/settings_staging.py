9c3427aa15a9245f7d65571b15daa8a307d8c98a
Jenny Thao Huynh <C22448184@mytudublin.ie>
2026-04-19 02:22:36 +0100

    Try fix CORS preflight issue in staging

--- backend/backend/settings_staging.py
+++ backend/backend/settings_staging.py
@@ -33,6 +33,26 @@ DATABASES = {
 # CORS / CSRF
 # -----------------------------
 FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost:5173")
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 CORS_URLS_REGEX = r"^/.*$" # Apply CORS to all endpoints
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
